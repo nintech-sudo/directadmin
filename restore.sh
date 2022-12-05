da398e55be3d2916a10d0302b020ffef299b37ce
NINTECH <97419597+nintech-sudo@users.noreply.github.com>
2022-12-05 12:55:38 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -17,7 +17,7 @@ if [ ! -s /usr/local/directadmin/conf/license.key ]; then
 fi
 
 if [ ${#array_list_file_backup_tar_gz[@]} -eq 0 ]; then
-    echo -e "No such file backup!\n"
+    echo -e "\e[0;31mNo such file backup!\e[0m\n"
     return 
 else
     for ((i = 0; i < ${#array_list_file_backup_tar_gz[@]}; i++)); do
