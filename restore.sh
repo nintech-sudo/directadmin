2cf537cd6b819b33897b1610b1710e11f07be77b
NINTECH <97419597+nintech-sudo@users.noreply.github.com>
2022-12-18 00:24:42 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -22,18 +22,13 @@ if [ ${#array_list_file_backup_tar_gz[@]} -eq 0 ]; then
 else
     for ((i = 0; i < ${#array_list_file_backup_tar_gz[@]}; i++)); do
 
-        gunzip /home/admin/admin_backups/file_backup/${array_list_file_backup_tar_gz[i]}
-        #file_backup_tar=$(echo ${array_list_file_backup_tar_gz[i]} | awk -F"." '{print $1"."$2"."$3"."$4}')
+        gunzip /home/admin/admin_backups/file_backup/${array_list_file_backup_tar_gz[i]}        
         file_backup_tar=$(find /home/admin/admin_backups/file_backup -type f -name "*.tar" | awk -F"/" '{print $6}')
         tar -xvf /home/admin/admin_backups/file_backup/$file_backup_tar -C /home/admin/admin_backups/file_backup/ backup/user.conf
         tar -vf /home/admin/admin_backups/file_backup/$file_backup_tar --delete backup/user.conf
         sed -i "s/ip=.*/ip=$ip_address/g" /home/admin/admin_backups/file_backup/backup/user.conf
         tar -uvf /home/admin/admin_backups/file_backup/$file_backup_tar -C /home/admin/admin_backups/file_backup/ backup/user.conf
         rm -rf /home/admin/admin_backups/file_backup/backup
-
-        #type=$(echo ${array_list_file_backup_tar_gz[i]} | awk -F"." '{ print $1}')
-        #owner=$(echo ${array_list_file_backup_tar_gz[i]} | awk -F"." '{ print $2}')
-        #user=$(echo ${array_list_file_backup_tar_gz[i]} | awk -F"." '{ print $3}')
         chown admin:admin /home/admin/admin_backups/file_backup/$file_backup_tar
         echo "action=restore&ip%5Fchoice=file&local%5Fpath=%2Fhome%2Fadmin%2Fadmin%5Fbackups%2Ffile%5Fbackup&owner=admin&select%30=$file_backup_tar&type=admin&value=multiple&when=now&where=local" >>/usr/local/directadmin/data/task.queue
         /usr/local/directadmin/dataskq d2000
