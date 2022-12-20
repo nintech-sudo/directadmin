7b85d938daa4e79fbab901308fc86de155528637
NINTECH <phamquangninh99@gmail.com>
2022-12-20 23:42:10 +0700

    Update da_script.sh

--- da_script.sh
+++ da_script.sh
@@ -453,8 +453,36 @@ function system_info() {
 
 function createSwap() {
 
-	list_swapon=($(swapon -s | awk '{print $(NR=1)}' | grep -v "Filename" | awk 'BEGIN{ORS=" "}1'))
+	function setSwap() {
 
+		#Xoa swap neu da ton tai
+		if [ ! ${#list_swapon[@]} -eq 0 ]; then
+			echo -e "Removing installed Swap "
+			for ((i = 0; i < ${#list_swapon[@]}; i++)); do
+
+				swapoff -v ${list_swapon[i]}
+				sed -i '/swap/d' /etc/fstab
+				rm -rf ${list_swapon[i]}
+			done
+		fi
+		dd if=/dev/zero of=/swapfile bs=1024 count=$(expr $select \* 1024)
+		chown root:root /swapfile
+		chmod 600 /swapfile
+		mkswap /swapfile
+		swapon /swapfile
+		echo "/swapfile   none    swap    sw    0   0" | sudo tee -a /etc/fstab
+		sed -i '/vm.swappiness/d' /etc/sysctl.conf
+		sysctl vm.swappiness=10
+		echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
+		sed 's/vm.swappiness =.*/vm.swappiness = 10/g' /usr/lib/tuned/virtual-guest/tuned.conf
+		echo -e "Show Ram Infomation...\n"
+		sleep 2
+		free -h
+		echo ""
+
+	}
+
+	list_swapon=($(swapon -s | awk '{print $(NR=1)}' | grep -v "Filename" | awk 'BEGIN{ORS=" "}1'))
 	echo -e "Show Ram Infomation...\n"
 	sleep 2
 	free -h
@@ -495,34 +523,6 @@ function createSwap() {
 		esac
 	done
 
-	function setSwap() {
-
-		#Xoa swap neu da ton tai
-		if [ ! ${#list_swapon[@]} -eq 0 ]; then
-			echo -e "Removing installed Swap "
-			for ((i = 0; i < ${#list_swapon[@]}; i++)); do
-
-				swapoff -v ${list_swapon[i]}
-				sed -i '/swap/d' /etc/fstab
-				rm -rf ${list_swapon[i]}
-			done
-		fi
-		dd if=/dev/zero of=/swapfile bs=1024 count=$(expr $select \* 1024)
-		chown root:root /swapfile
-		chmod 600 /swapfile
-		mkswap /swapfile
-		swapon /swapfile
-		echo "/swapfile   none    swap    sw    0   0" | sudo tee -a /etc/fstab
-		sed -i '/vm.swappiness/d' /etc/sysctl.conf
-		sysctl vm.swappiness=10
-		echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf
-		sed 's/vm.swappiness =.*/vm.swappiness = 10/g' /usr/lib/tuned/virtual-guest/tuned.conf
-		echo -e "Show Ram Infomation...\n"
-		sleep 2
-		free -h
-		echo ""
-
-	}
 }
 
 function speed_test() {
