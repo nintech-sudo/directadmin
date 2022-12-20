92fa4d62e278ef8d88bd8fe2e7fd277179d0cd5e
NINTECH <phamquangninh99@gmail.com>
2022-12-20 14:43:49 +0700

    Update convert_cp_to_da.sh

--- Convert-Cpanel-to-Directadmin/convert_cp_to_da.sh
+++ Convert-Cpanel-to-Directadmin/convert_cp_to_da.sh
@@ -72,8 +72,8 @@ sed -i 's/ip=.*/ip=$ip_vps/g' $Dir_Path'defaults.conf'
 
 perl da.cpanel.import.pl
 
-if [ -e $Dir_Path'export/*' ]; then 
-    mv $Dir_Path'export/*' $Dir_Path'file_backup/'
+if [ -e $Dir_Path'export/'* ]; then 
+    mv $Dir_Path'export/'* $Dir_Path'file_backup/'
 fi
 #rm -rf $Dir_Path'export/'
 rm -rf $Dir_Path'da.cpanel.import.pl'
