514030e83be8cd449eff1edf4a008eacc8c72b8d
NINTECH <97419597+nintech-sudo@users.noreply.github.com>
2022-12-22 23:14:36 +0700

    Update convert_cp_to_da.sh

--- Convert-Cpanel-to-Directadmin/convert_cp_to_da.sh
+++ Convert-Cpanel-to-Directadmin/convert_cp_to_da.sh
@@ -63,7 +63,7 @@ curl -O https://raw.githubusercontent.com/nintech-sudo/directadmin/main/Convert-
 curl -O https://raw.githubusercontent.com/nintech-sudo/directadmin/main/Convert-Cpanel-to-Directadmin/defaults.conf
 chmod +x da.cpanel.import.pl
 ip_vps=$(curl -s http://ip.vinahost.vn)
-sed -i 's/ip=.*/ip=${ip_vps}/g' $Dir_Path'defaults.conf'
+sed -i 's/ip=.*/ip='$ip_vps'/' $Dir_Path'defaults.conf'
 
 /usr/local/directadmin/directadmin set max_username_length 30 restart >/dev/null
 /usr/local/directadmin/directadmin set mysql_use_new_user_methods 1 restart >/dev/null
